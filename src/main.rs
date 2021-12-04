2dc84d90028ef328f3941699b3a9e1540e07f2ad
Serhii Bura <serhiibura@gmail.com>
2021-12-05 01:22:26 +0200

    src: Create empty application
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{executor, Application, Command, Element, Settings, Text, Executor, Clipboard};
+
+#[derive(Debug)]
+struct Generator;
+
+impl Application for Generator {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Generator, Command<Self::Message>) {
+        (Generator, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool application")
+    }
+
+    fn update(&mut self, _message: Self::Message, _clipboard: &mut Clipboard) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
+}
+
+fn main() -> iced::Result {
+    Generator::run(Settings::default())
 }
